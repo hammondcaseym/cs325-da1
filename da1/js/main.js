dab6ed2a2fbd79d4b68ac28645d179f52be20658
casey hammond <chammon7@gmu.edu>
2019-02-07 12:41:56 -0500

    added in mangoes, point system, timer, and mango count.  Also added a 'finishing image' that tells you your score.  Also added more mangoes if the screen is cleared of mangoes before the time runs out so you can rack up more points than just 2000

--- da1/js/main.js
+++ da1/js/main.js
@@ -22,6 +22,8 @@ window.onload = function() {
 		game.load.spritesheet('button', 'buttons/button_click.png', 128, 64);
 		game.load.spritesheet('flamango', 'Sprites/Mango_Idle_Sheet.png', 32, 64, 2);
 		game.load.image('background', 'Sprites/background.png');
+		game.load.spritesheet('item', 'Sprites/mango.gif', 32,32);
+		game.load.spritesheet('finish','Sprites/finish.gif',800,600);
     }
     
 	
@@ -29,11 +31,13 @@ window.onload = function() {
 	var bg;
     var button;
 	var text;
+	var direction = 1;
     function create() {
 		 
         // Create a sprite at the center of the screen using the 'logo' image.
 		
-		bg = game.add.sprite(game.world.centerX - 400, game.world.centerY - 300, 'bg');
+		//bg = game.add.sprite(game.world.centerX - 400, game.world.centerY - 300, 'bg');
+		game.stage.backgroundColor = '#182d3b';
 		button = game.add.button(game.world.centerX - 64, 400, 'button', startGame, this, 2, 1, 0);  
 																	//1 = idle, 0 = hover, 2 = click
         bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
@@ -62,7 +66,7 @@ window.onload = function() {
 		//get rid of previous assets as to not cick on buttons after use
 		button.pendingDestroy = true;
 		console.log('button destroyed!');
-		bg.destroy();
+		//bg.destroy();
 		console.log('background1 removed');
 		text.destroy();
 		console.log('title destroyed!');
@@ -72,6 +76,9 @@ window.onload = function() {
 		loadNew(); //go to new scene
 	}
 	var mango;
+	var item;
+	var items;
+	var gameStarted = false;
 	function loadNew(){
 				//load new scene
 		game.add.sprite(game.world.centerX - 400, game.world.centerY - 300, 'background');
@@ -82,19 +89,84 @@ window.onload = function() {
 		
 	}
 	var jumpTimer = 0;
+	var points = 0;
+	var mangoes = 0;
+	var pointText;
+	var mangoText;
+	var timer_ms;
+	var timer = 20;
+	var timer_text;
+	var timer_label;
+	var itemCount = 20;
 	function beginPlay(){
 		
 		game.time.desiredFps = 30;
 		//game.physics.arcade.gravity.y = 200;
+		//the following grouping and randomly placing sprites code is from https://codepen.io/Samid737/pen/RLQBqM?editors=1010
+		
+		placeItems();
+			
+		//item = game.add.sprite(game.world.centerX, game.world.centerY, 'item');
 		mango = game.add.sprite(game.world.centerX-280, game.world.centerY-150, 'flamango');
 		game.physics.enable(mango, Phaser.Physics.ARCADE );
 		//mango.body.bounce.y = 0.1;
+		mango.anchor.setTo(0.5,0.5);
 		mango.body.collideWorldBounds = true;
 		//mango.scale.setTo(2,2);
 		var idle = mango.animations.add('idle');
 		mango.animations.play('idle', 2, true);
+		gameStarted = true;
+		var style = { font: "25px Verdana", fill: "#9999ff", align: "left" };
+		text = game.add.text( game.world.centerX-200, 15, "Points:  ", style );
+        text.anchor.setTo( 0.5, 0.0 );
+		text = game.add.text( game.world.centerX+200, 15, "Mangoes:  ", style );
+        text.anchor.setTo( 0.5, 0.0 );
+		pointText = game.add.text( game.world.centerX-140, 15, points, style );
+        pointText.anchor.setTo( 0.2, 0.0 );
+		mangoText = game.add.text( game.world.centerX+270, 15, mangoes, style );
+        mangoText.anchor.setTo( 0.2, 0.0 );
+		timer_label = game.add.text( game.world.centerX-40, 15, "Time:  ", style );
+        timer_label.anchor.setTo( 0.2, 0.0 );
+		timer_text = game.add.text( game.world.centerX+25, 15, timer, style );
+        timer_text.anchor.setTo( 0.2, 0.0 );
+		timer_ms = game.time.create(false);
+		timer_ms.loop(1500,update_ms,this);
+		
+		timer_ms.start();
 
 	}
+	
+	
+	function placeItems(){
+		
+		var placeScale = 0.85;
+		items = game.add.group();
+		items.createMultiple(20,'item',null,true);
+		items.forEach(function(item){
+			item.position=new Phaser.Point(
+				Math.random()*game.width*placeScale+game.width*(1.0-placeScale)/2.0,
+				Math.random()*game.height*placeScale+game.width*(1.0-placeScale)/2.0
+				)},this);
+				console.log("items placed!");
+		
+	}
+	function update_ms(){
+		if(timer > 0){
+			timer --;
+		} else {
+			endGame();
+		}
+		
+	}
+	var finish;
+	var score;
+	function endGame(){
+		finish = game.add.sprite(game.world.centerX-400, game.world.centerY-300, 'finish');
+		var style = { font: "35px Verdana", fill: "#FFFFFF", align: "center" };
+		score = game.add.text( game.world.centerX-100, 265, "SCORE:  ", style);
+		var final = game.add.text(game.world.centerX + 50, 265, points, style);
+	}
+
 	
 	function goFS(){
 		    if (game.scale.isFullScreen)
@@ -106,6 +178,8 @@ window.onload = function() {
         game.scale.startFullScreen(false);
     }
 	}
+	
+	
     
     function update() {
         // Accelerate the 'logo' sprite towards the cursor,
@@ -115,26 +189,54 @@ window.onload = function() {
         // new trajectory.
         bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, game.input.activePointer, 500, 500, 500 );
     
-	
-    if (game.input.keyboard.isDown(Phaser.Keyboard.A))
-    {
-        mango.x -= 4;
-    }
-    else if (game.input.keyboard.isDown(Phaser.Keyboard.D))
-    {
-        mango.x += 4;
-    }
+		
+		if (game.input.keyboard.isDown(Phaser.Keyboard.A))
+		{
+			if(direction==0) {
+				direction = 1;
+				mango.scale.x *= -1;
+			}
+			mango.x -= 4;
+		}
+		else if (game.input.keyboard.isDown(Phaser.Keyboard.D))
+		{
+			if (direction==1) {
+				direction=0;
+				mango.scale.x *= -1;
+			}
+			mango.x += 4;
+		}
 
-    if (game.input.keyboard.isDown(Phaser.Keyboard.W))
-    {
-        mango.y -= 4;
-    }
-    else if (game.input.keyboard.isDown(Phaser.Keyboard.S))
-    {
-        mango.y += 4;
-    }
-	
-	
+		if (game.input.keyboard.isDown(Phaser.Keyboard.W))
+		{
+			mango.y -= 4;
+		}
+		else if (game.input.keyboard.isDown(Phaser.Keyboard.S))
+		{
+			mango.y += 4;
+		}
+		if (gameStarted) {
+			timer_text.setText(timer);
+			items.forEach(function(item){
+				if((Math.sqrt((item.x - mango.x)*(item.x - mango.x) + (item.y - mango.y)*(item.y - mango.y)) <= 30.0) && timer > 0) {
+					console.log("Within Proximity!");
+					item.destroy();
+					points += 100;
+					pointText.setText(points);
+					console.log(points);
+					mangoes += 1;
+					mangoText.setText(mangoes);
+					console.log(mangoes);
+					itemCount--;
+					console.log("item count = " + itemCount);
+					if(itemCount ==0) {
+						placeItems();
+						itemCount = 20;
+					}
+					
+				}
+			},this);
+		}
 	}
 	
 	function render () {
